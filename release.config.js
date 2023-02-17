cb297d9e28446ba86c052e3b4460cbeae6da9b9a
Zhipeng Chang <zhipeng.chang@etlsolutions.com>
2023-02-17 15:43:30 +0000

    fix bug

--- release.config.js
+++ release.config.js
@@ -18,7 +18,7 @@ const config = {
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    "@semantic=release/github",
+    "@semantic-release/github",
   ],
 };
 
